df4fe1b7f59065a2392ce4b309c44a9d07d50bd8
Florian Nitschmann <f.nitschmann@media-n.net>
2011-12-04 21:04:23 +0100

    Upgrade to Version 2.0

--- MustacheApi.php
+++ MustacheApi.php
@@ -6,11 +6,14 @@
 	 * It uses the WideImage PHP Libary (http://wideimage.sourceforge.net/) and Face.com Rest API PHP Library.
 	 *
 	 * @author Florian Nitschmann (info@florian-nitschmann.de)
+	 * @date 04-12-11 9:00:00pm
 	 * @links www.florian-nitschmann.de
 	 * @copyright (C) 2011 Florian Nitschmann
-	 * @license MIT License (http://www.opensource.org/licenses/mit-license.php)  
+	 * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+	 * @version 2.0   
 	 */
-	//Check for GD-Libary
+	
+	//Check if the PHP GD-Libary is available
 	if(!function_exists('gd_info')) throw new Exception('Mustache Face API requires the PHP GD extension.');
 	//includes
 	include_once('lib/FaceRestClient.php');
@@ -23,7 +26,14 @@
 		//Mustache images
 		public $mustache_images;
 
-
+		/*
+		 * __construct
+		 *
+		 * @access public
+		 * @param String $api_key [required] - The key for the Face.com API
+		 * @param String $api_secret [required] - The secret key for the Face.com API 
+		 * @param String $image_folder [optional] - The realpath to the mustache images could be found
+		 */
 		public function __construct($api_key, $api_secret, $image_folder = null) {
 			//Create new FaceRestClient() Instance
 			$this->FaceClient = new FaceRestClient($api_key, $api_secret);
@@ -52,61 +62,43 @@
 			}
 		}
 
-		
-		public function mustacheFromUrl($url, $mustache_type = 1) {
-			if($url != '') {
+
+		/*
+		 * detectMustacheImage - Detects all faces in a image from an url via the Face.com API and returns an with image infos, the detection result and a WideImage Image Object
+		 * 
+		 * @access public
+		 * @param String $url [required] - The url where the image exists
+		 * @return array   
+		 */
+		public function detectMustacheImage($url = null) {
+			if($url != null) {
 				$face_result = $this->FaceClient->faces_detect($url);
-				//New Image Infos
-				$new_img_info = array(
+
+				//Image infos for new pic
+				$new_img = array(
 					'width' => $face_result['photos']['0']['width'],
-					'height' => $face_result['photos']['0']['height'],
-					'type' => getimagesize($url)
+					'height' => $face_result['photos']['0']['width'],
+					'type' => getimagesize($url),
+					'face_api_result' => $face_result
 				);
-				//Create new WideImage
-				$new_img = WideImage::load($url)->resize($new_img_info['width'], $new_img_info['height']);
-				//Return WideImage Object with mustache
-				return $this->mustachePic($new_img, $new_img_info, $face_result, $mustache_type);
-			}
-			else return false;
-		}
-		
-
-		public function mustachePicHtml($mustache_obj, $w = null, $h = null) {
-			if($mustache_obj != null) {
-				if($w > 0 && $h > 0) $img = $mustache_obj->resize($w, $h);
-				else $img = $mustache_obj;
-				$img = imagecreatefromstring($img->asString('jpg')); 
-
-				$img = base64_encode(WideImage::load($img));
-				$string = 'data:image/jpeg;base64,'.$img;
-				return $string;
+				//WideImage
+				$wide_image_obj = WideImage::load($url)->resize($new_img['width'], $new_img['height']);
+				$new_img['wide_image_obj'] = $wide_image_obj;
+				//Return 
+				return $new_img;
 			}
 			else return null;
 		}
 
 
-		public function mustacheOutputPic($mustache_obj, $w = null, $h = null) {
-			if($mustache_obj != null) {
-				if($w > 0 && $h > 0) $img = $mustache_obj->resize($w, $h);
-				else $img = $mustache_obj;
-				//output image as jpeg
-				$img->output('jpg');
-			}
-			else return null; 
-		}
-
-
-		public function mustachePicRaw($mustache_obj,$w = null, $h = null) {
-			if($mustache_obj != null) {
-				if($w > 0 && $h > 0) $img = $mustache_obj->resize($w, $h)->asString('jpg');
-				else $img = $mustache_obj->asString('jpg');
-				//return string
-				return $img;
-			}
-			else return null;	
-		}
-
-		public function imgStringToObj($img_string = null) {
+		/*
+		 * imgStringToImageObj - Turns a raw and real image string into a WideImage Object 
+		 *
+		 * @access public
+		 * @param String $img_string [required] - The raw image string [In every supported format]
+		 * @return Object [WideImage Object]
+		 */
+		public function imgStringToImageObj($img_string = null) {
 			if($img_string != null) {
 				$mustache_obj = WideImage::load($img_string);
 				return $mustache_obj;
@@ -114,11 +106,64 @@
 			else return null;
 		}
 
-		protected function mustachePic($img, $img_info = array(), $face_result = array(), $mustache_type) {
+	
+		/*
+		 * urlImageToImageObj - Turns an image from an url directly in a WideImage Object with new size (optional)
+		 *
+		 * @access public
+		 * @param String $url [required] - The url of the image
+		 * @param int $w [optional] - The new result width of the image
+		 * @param int $h [optional] - The new result height of the image
+		 * @return Object [WideImage Object]
+		 */
+		public function urlImageToImageObj($url, $w = null, $h = null) {
+			if($url != null) {
+				if($w > null && $h > null) $wide_image_obj = WideImage::load($url)->resize($w, $h);
+				else $wide_image_obj = WideImage::load($url);
+				//Return 
+				return $wide_image_obj;
+			}
+			else return null;
+		}
+
+
+		/*
+		 * mustacheImageFromUrl - Creates a image with mustaches directly from a url and returns result as WideImage Object
+		 *
+		 * @access public
+		 * @param  String $url [required] - The image url 
+		 * @param Char $mustache_type [optional] - The mustache image, wich is checked over the mustache images folder. [defualt = r (random)]
+		 * @return Object [WideImage Object]
+		 */
+		public function mustacheImageFromUrl($url, $mustache_type = 'r') {
+			if($url != '') {
+				$result = $this->detectMustacheImage($url);
+				//Image info
+				$image_info = array(
+					'width' => $result['width'],
+					'height' => $result['height'],
+					'type' => $result['type']
+				);
+				//Return WideImage Object
+				return $this->mustachePic($result['wide_image_obj'], $image_info, $result['face_api_result'], $mustache_type);
+			}
+			else return null;
+		}
+
+		/*
+		 * mustachePic - Creates (if possible) the mustaches on a WideImage Image Object
+		 * 
+		 * @access public
+		 * @param Object $img [required] - The WideImage Object 
+		 * @param array $img_info [required] - Infos about the image (Width, Height, Type)
+		 * @param array $face_result [required] - All detected details of the Face.com API
+		 * @param Char $mustche_type [optional] - See mustachePicFromUrl()
+		 * @return Object [WideImage Object]
+		 */
+		public function mustachePic($img, $img_info = array(), $face_result = array(), $mustache_type = 'r') {
 			if($img != '' && is_array($img_info) && is_array($face_result) && $mustache_type != '') {
 				$mime = $img_info['type']['mime'];
-				
-				//Create pic extension based $mime
+				//Create pic extension based on $mime
 				$extension;
 				if($mime = "image/jpeg") $extension = 'jpeg';
 				else if($mime = "image/png") $extension = 'png';
@@ -193,6 +238,68 @@
 			else return null;
 		} 
 
-		
+
+		/*
+		 * outputMustachePicRaw - Outputs a WideImage directly in the browser as JPEG image
+		 *
+		 * @access public
+		 * @param Object $mustache_obj [required] - A WideImage Object. If it was not detected via mustachePic() it will output a pic without any mustaches
+		 * @param int $w [optional] - The new width of the image
+		 * @param int $h [optional] - The new height of the image
+		 */
+		public function outputMustachePicRaw($mustache_obj, $w = null, $h = null) {
+			if($mustache_obj != null) {
+				if($w > 0 && $h > 0) $img = $mustache_obj->resize($w, $h);
+				else $img = $mustache_obj;
+				//output image as jpeg
+				$img->output('jpg');
+			}
+			else return null; 
+		}
+
+
+		/*
+		 * returnMustachePicString - Returns a raw image string in JPEG format
+		 *
+		 * @access public
+		 * @param Object $mustache_obj [required] - A WideImage Object. If it was not detected via mustachePic() it will output a pic without any mustaches
+		 * @param int $w [optional] - The new width of the image
+		 * @param int $h [optional] - The new height of the image
+		 * @return String
+		 */
+		public function returnMustachePicString($mustache_obj,$w = null, $h = null) {
+			if($mustache_obj != null) {
+				if($w > 0 && $h > 0) $img = $mustache_obj->resize($w, $h)->asString('jpg');
+				else $img = $mustache_obj->asString('jpg');
+				//Return the image string
+				return $img;
+			}
+			else return null;	
+		}
+
+
+		/*
+		 * returnMustachePicHtml - Returns a string that can be used directly in your HTML source code in the <img> tag. ATTENTION: The string will be a complete Base64 encoded string in your source code, may slow down the speed and will not work in older IE browsers! If you want to attempt DO NOT use this function
+		 *
+		 * @access public
+		 * @param Object $mustache_obj [required] - A WideImage Object. If it was not detected via mustachePic() it will output a pic without any mustaches
+		 * @param int $w [optional] - The new width of the image
+		 * @param int $h [optional] - The new height of the image
+		 * @return String [Base64 Encoded Image]
+		 */
+		public function returnMustachePicHtml($mustache_obj, $w = null, $h = null) {
+			if($mustache_obj != null) {
+				if($w > 0 && $h > 0) $img = $mustache_obj->resize($w, $h);
+				else $img = $mustache_obj;
+				$img = imagecreatefromstring($img->asString('jpg')); 
+
+				$img = base64_encode(WideImage::load($img));
+				$string = 'data:image/jpeg;base64,'.$img;
+				return $string;
+			}
+			else return null;
+		}
+
+	//---End MustacheApi	
 	}
 ?>
